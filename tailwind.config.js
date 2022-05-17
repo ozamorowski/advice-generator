501668c37b48eac6a90e3afeb8e3022bd2f6c8e4
Oskar Zamorowski <ozamorowski@gmail.com>
2022-05-17 15:56:56 +0200

    Active states.

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,9 @@ module.exports = {
         'grayish-blue': "hsl(217, 19%, 38%)",
         'light-cyan': "hsl(193, 38%, 86%)",
         'neon-green': "hsl(150, 100%, 66%)"
+      },
+      boxShadow: {
+        '3xl': '0 0 60px 10px rgba(82, 255, 168, 0.4)',
       }
     },
   },
